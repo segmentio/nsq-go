5a730d186e6cec53c641722dd13556ae7b49f294
Amir Abushareb <yields@icloud.com>
2017-03-23 23:53:13 -0700

    identify: add MsgTimeout

--- identify_test.go
+++ identify_test.go
@@ -1,11 +1,21 @@
 package nsq
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestIdentify(t *testing.T) {
 	testCommand(t, "IDENTIFY", Identify{
 		ClientID:  "0123456789",
 		Hostname:  "localhost",
 		UserAgent: "nsq-go/test",
+	})
+
+	testCommand(t, "IDENTIFY", Identify{
+		ClientID:   "0123456789",
+		Hostname:   "localhost",
+		UserAgent:  "nsq-go/test",
+		MsgTimeout: time.Second * 5,
 	})
 }

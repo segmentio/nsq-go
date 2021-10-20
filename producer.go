segmentio/nsq-go

commit 5014dbba9fcf996c0023a9c7956a93431f2eda00

Return connection errors for producer requests (#46)
Producer now internally tracks NSQ connection errors, so that while it
is not connected to NSQ, the errors are returned for message publication
requests. Before this change, Producer would wait indefinitely to
publish a message until the connection was established.

This partially reverts some changes in commit 1d78b8b (but still leaves
out the old force-connect option).

Also:

The DST Root CA X3 certificate used by Let's Encrypt expired at the end
of September 2021. This change removes it from the CA certificates
available in the build image used by CircleCI so that building and testing work
once again.

producer.go
@@ -216,7 +216,6 @@ func (p *Producer) stop() {
 
 func (p *Producer) run() {
 	var conn *Conn
-	var reqChan <-chan ProducerRequest
 	var resChan chan Frame
 	var pending []ProducerRequest
 
@@ -227,7 +226,6 @@ func (p *Producer) run() {
 			close(resChan)
 			conn.Close()
 			conn = nil
-			reqChan = nil
 			resChan = nil
 			pending = completeAllProducerRequests(pending, err)
 		}
@@ -245,7 +243,6 @@ func (p *Producer) run() {
 			return
 		}
 
-		reqChan = p.reqs
 		resChan = make(chan Frame, 16)
 		go p.flush(conn, resChan)
 
@@ -259,7 +256,7 @@ func (p *Producer) run() {
 	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()
 
-	connect()
+	connErr := connect()
 
 	for {
 		select {
@@ -268,7 +265,7 @@ func (p *Producer) run() {
 
 		case now := <-ticker.C:
 			if conn == nil {
-				connect()
+				connErr = connect()
 			}
 
 			if producerRequestsTimedOut(pending, now) {
@@ -276,10 +273,15 @@ func (p *Producer) run() {
 				continue
 			}
 
-		case req, ok := <-reqChan:
+		case req, ok := <-p.reqs:
 			if !ok && req.Topic == "" {
-				// exit if reqChan is closed and there are no more requests to process
+				// exit if the request channel is closed and there are no more requests to process
 				return
+			}
+
+			if connErr != nil {
+				req.complete(connErr)
+				continue
 			}
 
 			if err := p.publish(conn, req.Topic, req.Message); err != nil {

.circleci/config.yml
@@ -51,6 +51,8 @@ jobs:
 
     steps:
       - checkout
+      - run: sudo rm -f /usr/share/ca-certificates/mozilla/DST_Root_CA_X3.crt
+      - run: sudo update-ca-certificates
       - run: go mod download
       - run: go test -v -race ./...
 
@@ -67,6 +69,8 @@ jobs:
       - run:
           name: Build Programs
           command: |
+            sudo rm -f /usr/share/ca-certificates/mozilla/DST_Root_CA_X3.crt
+            sudo update-ca-certificates
             go mod download
             go build ./cmd/nsq-to-nsq
             go build ./cmd/nsq-to-http

commit b1eab10799087e55772e09eb673c8b941a4a8ac1

Make connection errors for producer optional
The new ability for producers to return connection errors introduced in
PR #46 is made optional. The default behavior for producers reverts to
what it was: blocking the submission of messages on connection failure.
When the new `FailOnConErr` flag in `ProducerConfig` is set to `true`,
then the new error support is active instead.

producer.go
@@ -15,6 +15,7 @@ type ProducerConfig struct {
 	Address        string
 	Topic          string
 	MaxConcurrency int
+	FailOnConnErr  bool
 	DialTimeout    time.Duration
 	ReadTimeout    time.Duration
 	WriteTimeout   time.Duration
@@ -54,11 +55,12 @@ type Producer struct {
 	started bool
 
 	// Immutable state of the producer.
-	address      string
-	topic        string
-	dialTimeout  time.Duration
-	readTimeout  time.Duration
-	writeTimeout time.Duration
+	address       string
+	topic         string
+	failOnConnErr bool
+	dialTimeout   time.Duration
+	readTimeout   time.Duration
+	writeTimeout  time.Duration
 }
 
 // ProducerRequest are used to represent operations that are submitted to
@@ -75,13 +77,14 @@ func NewProducer(config ProducerConfig) (p *Producer, err error) {
 	config.defaults()
 
 	p = &Producer{
-		reqs:         make(chan ProducerRequest, config.MaxConcurrency),
-		done:         make(chan struct{}),
-		address:      config.Address,
-		topic:        config.Topic,
-		dialTimeout:  config.DialTimeout,
-		readTimeout:  config.ReadTimeout,
-		writeTimeout: config.WriteTimeout,
+		reqs:          make(chan ProducerRequest, config.MaxConcurrency),
+		done:          make(chan struct{}),
+		address:       config.Address,
+		topic:         config.Topic,
+		failOnConnErr: config.FailOnConnErr,
+		dialTimeout:   config.DialTimeout,
+		readTimeout:   config.ReadTimeout,
+		writeTimeout:  config.WriteTimeout,
 	}
 
 	return
@@ -216,6 +219,8 @@ func (p *Producer) stop() {
 
 func (p *Producer) run() {
 	var conn *Conn
+	var connErr error
+	var reqChan <-chan ProducerRequest
 	var resChan chan Frame
 	var pending []ProducerRequest
 
@@ -226,6 +231,10 @@ func (p *Producer) run() {
 			close(resChan)
 			conn.Close()
 			conn = nil
+			connErr = errors.New("connection shutdown")
+			if !p.failOnConnErr {
+				reqChan = nil
+			}
 			resChan = nil
 			pending = completeAllProducerRequests(pending, err)
 		}
@@ -243,6 +252,7 @@ func (p *Producer) run() {
 			return
 		}
 
+		reqChan = p.reqs
 		resChan = make(chan Frame, 16)
 		go p.flush(conn, resChan)
 
@@ -256,7 +266,10 @@ func (p *Producer) run() {
 	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()
 
-	connErr := connect()
+	if p.failOnConnErr {
+		reqChan = p.reqs
+	}
+	connErr = connect()
 
 	for {
 		select {
@@ -273,13 +286,13 @@ func (p *Producer) run() {
 				continue
 			}
 
-		case req, ok := <-p.reqs:
+		case req, ok := <-reqChan:
 			if !ok && req.Topic == "" {
-				// exit if the request channel is closed and there are no more requests to process
+				// exit if reqChan is closed and there are no more requests to process
 				return
 			}
 
-			if connErr != nil {
+			if p.failOnConnErr && connErr != nil {
 				req.complete(connErr)
 				continue
 			}